c43311e00acf586365cf59131b29ef0ce2b17191
wisie <wisnia501@gmail.com>
2024-11-12 16:38:46 +0100

    add 06 ppj initial

---
+++ 1 semester/PPJ/06/02/S34380.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class S34380 {
+  public void method() {
+    int[] tablica = {};
+    Random losu = new Random();
+    for (int i=0; i<10; i++) {
+      int next = losu.nextInt(2);
+      tablica[i] = next;
+    }
+    for (int i=0; i<tablica.length; i++) System.out.println(tablica[i]);
+  }
+}
---
+++ 1 semester/PPJ/06/04/S34380.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+
+public class S34380 {
+  public void method() {
+    double[] tablica = {0,0,0,0,0,0,0,0,0,0};
+    Random losu = new Random();
+    for (int i=0; i < tablica.length; i++) {
+      double next = losu.nextDouble(10);
+      tablica[i] = next;
+    }
+
+    // wszystkie
+    System.out.println("Zawartość całej tablicy:");
+    String tablicaString = "";
+    for (int i=0; i < tablica.length; i++) {
+      tablicaString += tablica[i] + " ";
+    }
+    System.out.println(tablicaString);
+
+    // parzyste
+    System.out.println("Elementy tablicy o indeksie parzystym:");
+    tablicaString = "";
+    for (int i=0; i < tablica.length; i+=2) {
+      tablicaString += tablica[i] + " ";
+    }
+    System.out.println(tablicaString);
+
+    // elementy ktorych konwersja na int jest liczba nieparzysta
+    System.out.println("Elementy tablicy, których wartości przekonwertowane na int są nieparzyste:");
+    tablicaString = "";
+    for (int i=0; i < tablica.length; i++) {
+      if (((int)tablica[i] % 2) == 1) tablicaString += tablica[i] + " ";
+    }
+    System.out.println(tablicaString);
+  }
+}
