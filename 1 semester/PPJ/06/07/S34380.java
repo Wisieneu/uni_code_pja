4c6295c210c0ad161d9a9a4c502144af1359ace4
wisie <wisnia501@gmail.com>
2024-11-20 14:09:53 +0100

    update 06

---
+++ 1 semester/PPJ/06/07/S34380.java
@@ -0,0 +1,8 @@
+public class S34380 {
+  public void method() {
+    String[] slowa = {
+      "Ala", "kota", "ma", "ma", "a", "kot", "Ale"
+    };
+    System.out.println(slowa[0] + " " + slowa[2] + " " + slowa[1] + " " + slowa[4]  + " " + slowa[5] + " " + slowa[3] + " " + slowa[6]);
+  }
+}
